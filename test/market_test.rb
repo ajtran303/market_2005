d0a787f259d0f108857974c5a87f945bbd7a3823
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-10 10:16:44 -0600

    Add test Market#total_inventory

--- test/market_test.rb
+++ test/market_test.rb
@@ -105,4 +105,36 @@ class MarketTest < MiniTest::Test
     assert_equal [@item1], market.overstocked_items
   end
 
+  def test_it_can_get_total_inventory
+    @vendor3.stock(@item3, 10)
+
+    market = Market.new("South Pearl Street Farmers Market")
+
+    market.add_vendor(@vendor1)
+    market.add_vendor(@vendor2)
+    market.add_vendor(@vendor3)
+
+    expected_inventory =
+      {
+      @item1 => {
+        quantity: 100,
+        vendors: [@vendor1, @vendor3]
+      },
+      @item2 => {
+        quantity: 7,
+        vendors: [@vendor1]
+      },
+      @item4 => {
+        quantity: 50,
+        vendors: [@vendor2]
+      },
+      @item3 => {
+        quantity: 35,
+        vendors: [@vendor2, @vendor3]
+      },
+    }
+    assert_equal expected_inventory, market.total_inventory
+  end
+
+
 end
