a87434e9d3a2997be93290024a9d573630ae5cf1
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-10 10:47:40 -0600

    Add Market#overstocked_items

--- lib/market.rb
+++ lib/market.rb
@@ -38,4 +38,11 @@ class Market
     end
   end
 
+  def overstocked_items
+    total_inventory.reduce([]) do |overstock, (item, details)|
+      overstock << item if details[:quantity] > 50 && details[:vendors].size > 1
+      overstock
+    end
+  end
+
 end
