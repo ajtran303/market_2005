8dc2307ad425fb4dbfac496c662f5f6ca4da9345
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-10 09:41:27 -0600

    Add test Vendor#potential_revenue

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -50,4 +50,33 @@ class VendorTest < MiniTest::Test
     assert_equal expected_inventory, vendor.inventory
   end
 
+  def test_it_knows_potential_revenue
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    #=> #<Item:0x007f9c56740d48...>
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    #=> #<Item:0x007f9c565c0ce8...>
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    #=> #<Item:0x007f9c562a5f18...>
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    #=> #<Item:0x007f9c56343038...>
+
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    #=> #<Vendor:0x00007fe1348a1160...>
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    #=> #<Vendor:0x00007fe1349bed40...>
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    #=> #<Vendor:0x00007fe134910650...>
+    vendor3.stock(item1, 65)
+
+    assert_equal 29.75, vendor1.potential_revenue
+    assert_equal 345.00, vendor2.potential_revenue
+    assert_equal 48.75  , vendor3.potential_revenue
+  end
+
 end
